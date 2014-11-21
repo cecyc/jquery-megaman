0d42f3eb896931401d493bf9d463665929ff074e
cecyc <cecycorrea@gmail.com>
2014-11-20 19:56:53 -0600

    Finalizing animation

--- js/app.js
+++ js/app.js
@@ -3,20 +3,26 @@ $(document).ready(function() {
     $('.mega-still').hide();
     $('.mega-ready').show();
   })
-    .mouseleave(function() {
+  .mouseleave(function() {
     $('.mega-ready').hide();
     $('.mega-still').show();
   })
-  .mousedown(function() {
+  .mousedown(function() {   
     $('.mega-ready').hide();
     $('.mega-throwing').show();
-    $('.haduken').show();
-    // play hadouken sound
-    // show hadouken and animate it to the right of the screen
+    $('.haduken').finish().show()
+    .animate(
+      {'left': '800px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '360px');
+      }
+    );
   })
   .mouseup(function() {
     $('.mega-throwing').hide();
     $('.mega-ready').show();
-    // ryu goes back to his ready position
   });
-})
+});
+
